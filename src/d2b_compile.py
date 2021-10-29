78ac324ff399854dc6524cefdf3cdff1458211c2
Andrew Ross <andrew.ross.mail@gmail.com>
2021-10-29 16:21:07 -0400

    Update src/d2b_compile.py
    Update plugin to use the same parser configuration used by compile-dcm2bids-config

--- src/d2b_compile.py
+++ src/d2b_compile.py
@@ -1,7 +1,6 @@
 import argparse
-from pathlib import Path
 
-import compile_dcm2bids_config
+import compile_dcm2bids_config as cdc
 from d2b.hookspecs import hookimpl
 
 
@@ -10,19 +9,6 @@ __version__ = "1.0.0"
 
 @hookimpl
 def register_commands(subparsers: argparse._SubParsersAction) -> None:
-    parser = subparsers.add_parser("compile")
-    parser.add_argument(
-        "in_file",
-        nargs="+",
-        type=Path,
-        help="The JSON config files to combine",
-    )
-    parser.add_argument(
-        "-o",
-        "--out-file",
-        type=argparse.FileType("w", encoding="utf8"),
-        default="-",
-        help="The file to write the combined config file to. If not "
-        "specified outputs are written to stdout.",
-    )
-    parser.set_defaults(handler=compile_dcm2bids_config._handler)
+    _parser: argparse.ArgumentParser = subparsers.add_parser("compile")
+    # configure the parser by going through compile-dcm2bids-config
+    cdc._create_parser(_parser)
